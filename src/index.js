34ba440512985308daed6618e0da1a7239858d54
janeluck <janeluck158@gmail.com>
2016-03-09 16:56:26 +0800

    react demo

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@
 // main.js
 var React = require('react');
 var ReactDOM = require('react-dom');
+var $ = require('jquery');
 var names = ['jane', 'joe', 'crystal'];
 var titles = [
     'It\'s Work!',
-    'awesome'
+    'awesome',
+    names
 ];
 ReactDOM.render(
     <div>
@@ -67,4 +69,175 @@ ReactDOM.render(
     </NotesList>,
     document.getElementById('listDemo')
 );
+
+
+// ref: get real dom
+
+var MyComponent = React.createClass({
+    handleClick: function() {
+        this.refs.myTextInput.focus();
+    },
+    render: function() {
+        return (
+            <div>
+                <input type="text" ref="myTextInput" />
+                <input type="button" value="Focus the text input" onClick={this.handleClick} />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <MyComponent />,
+    document.getElementById('realDom')
+);
+
+
+// state: state machine
+var LikeButton = React.createClass({
+    getInitialState: function() {
+        return {liked: false};
+    },
+    handleClick: function(event) {
+        this.setState({liked: !this.state.liked});
+    },
+    render: function() {
+        var text = this.state.liked ? 'like' : 'haven\'t liked';
+        return (
+            <p onClick={this.handleClick}>
+                You {text} this. Click to toggle.
+            </p>
+        );
+    }
+});
+
+ReactDOM.render(
+    <LikeButton />,
+    document.getElementById('stateMachine')
+);
+
+
+// forms
+var FormEl0 = React.createClass({
+    getInitialState: function() {
+        return {value: 'Hello!'};
+    },
+    handleChange: function(event) {
+        this.setState({value: event.target.value});
+    },
+    render: function() {
+        return (
+            <input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}
+            />
+        );
+    }
+});
+ReactDOM.render(
+   <FormEl0 />,
+    document.getElementById('formEl0')
+
+);
+
+
+
+
+var FormEl1 = React.createClass({
+    getInitialState: function() {
+        return {value: 'C'};
+    },
+    handleChange: function(event) {
+        this.setState({value: event.target.value});
+    },
+    render: function(){
+        return   <select value="B" onChange={this.handleChange}>
+            <option value="A">Apple</option>
+            <option value="B">Banana</option>
+            <option value="C">Cranberry</option>
+        </select>
+    }
+});
+ReactDOM.render(
+    <FormEl1 />,
+    document.getElementById('formEl1')
+
+);
+
+
+
+var Notices = React.createClass({
+    getInitialState: function () {
+        return {
+            opacity: 1.0
+        };
+    },
+
+    componentDidMount: function () {
+        this.timer = setInterval(function () {
+            var opacity = this.state.opacity;
+            opacity -= .05;
+            if (opacity < 0.1) {
+                opacity = 1.0;
+            }
+            this.setState({
+                opacity: opacity
+            });
+        }.bind(this), 100);
+    },
+
+    render: function () {
+        return (
+            <div style={{opacity: this.state.opacity}}>
+                Hello {this.props.name}
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Notices name="world"/>,
+    document.getElementById('lifeCycle')
+
+);
+
+
+var UserGist = React.createClass({
+    getInitialState: function() {
+        return {
+            username: '',
+            lastGistUrl: ''
+        };
+    },
+
+    componentDidMount: function() {
+        $.get(this.props.source, function(result) {
+            var lastGist = result[0];
+            if (this.isMounted()) {
+                this.setState({
+                    username: lastGist.owner.login,
+                    lastGistUrl: lastGist.html_url
+                });
+            }
+        }.bind(this));
+    },
+
+    render: function() {
+        return (
+            <div>
+                {this.state.username}'s last gist is
+                <a href={this.state.lastGistUrl}>here</a>.
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <UserGist source="https://api.github.com/users/octocat/gists" />,
+    document.getElementById('ajaxDemo')
+);
+
+
+
 
